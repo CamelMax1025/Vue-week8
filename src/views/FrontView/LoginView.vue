4325631ebdf1afa38f1405a30e32b0e3c081c2ed
CamelLou <mod22344689@gmail.com>
2024-03-23 04:49:53 +0800

    fix-api-post-/path

--- src/views/FrontView/LoginView.vue
+++ src/views/FrontView/LoginView.vue
@@ -1,60 +1,72 @@
 <template>
-        <div class="container pt-7">
-        <h2>登入</h2>
-            <div class="row justify-content-center">
-                <h1 class="h3 mb-3 font-weight-normal">
-                </h1>
-                <div class="col-8">
-                <form id="form" class="form-signin" @submit.prevent="login">
-                    <div class="form-floating mb-3">
-                    <input type="email" class="form-control" id="username" v-model="user.username"
-                        placeholder="name@example.com" required autofocus>
-                    <label for="username">Email address</label>
-                    </div>
-                    <div class="form-floating">
-                    <input type="password" class="form-control" id="password" v-model="user.password"
-                        placeholder="Password" required>
-                    <label for="password">Password</label>
-                    </div>
-                    <button class="btn btn-lg btn-primary w-100 mt-3" type="submit">
-                    登入
-                    </button>
-                </form>
-                </div>
-            </div>
-            <p class="mt-5 mb-3 text-muted">
-            </p>
-            </div>
-    </template>
-    
-    <script>
-    import axios from 'axios';
-    
-    const { VITE_URL } = import.meta.env;
-    
-    export default {
-        data() {
-        return {
-            user: {
-            username: '',
-            password: '',
-            },
-        };
-        },
-        methods: {
-        login() {
-            const url = `${VITE_URL}/admin/signin`;
-            axios.post(url, this.user)
-            .then((response) => {
-                const { token, expired } = response.data;
-                document.cookie = `camelpath2=${token};expires=${new Date(expired)}; path=/`;
-                this.$router.push('/admin/products');
-            })
-            .catch((err) => {
-                alert(err.data.message);
-                this.$router.push('/login');
-            });
-        },
-        },
+  <div class="container pt-7">
+    <h2>登入</h2>
+    <div class="row justify-content-center">
+      <h1 class="h3 mb-3 font-weight-normal"></h1>
+      <div class="col-8">
+        <form id="form" class="form-signin" @submit.prevent="login">
+          <div class="form-floating mb-3">
+            <input
+              type="email"
+              class="form-control"
+              id="username"
+              v-model="user.username"
+              placeholder="name@example.com"
+              required
+              autofocus
+            />
+            <label for="username">Email address</label>
+          </div>
+          <div class="form-floating">
+            <input
+              type="password"
+              class="form-control"
+              id="password"
+              v-model="user.password"
+              placeholder="Password"
+              required
+            />
+            <label for="password">Password</label>
+          </div>
+          <button class="btn btn-lg btn-primary w-100 mt-3" type="submit">
+            登入
+          </button>
+        </form>
+      </div>
+    </div>
+    <p class="mt-5 mb-3 text-muted"></p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+const { VITE_URL } = import.meta.env;
+
+export default {
+  data() {
+    return {
+      user: {
+        username: '',
+        password: '',
+      },
     };
-  </script>
+  },
+  methods: {
+    login() {
+      const url = `${VITE_URL}/admin/signin`;
+      axios
+        .post(url, this.user)
+        .then((response) => {
+          const { token, expired } = response.data;
+          document.cookie = `camelpath2=${token};expires=${new Date(expired)};`;
+          this.$router.push('/admin/products');
+        })
+        .catch((err) => {
+          alert(err.data.message);
+          this.$router.push('/login');
+        });
+    },
+  },
+};
+</script>
