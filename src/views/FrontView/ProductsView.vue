a9448bf28973568d56bfe8bc700c2e87b48d56ad
CamelLou <mod22344689@gmail.com>
2024-03-23 04:19:22 +0800

    fix-img-url

--- src/views/FrontView/ProductsView.vue
+++ src/views/FrontView/ProductsView.vue
@@ -3,19 +3,12 @@
     class="position-relative d-flex align-items-center justify-content-center"
     style="min-height: 400px"
   >
-    <div
-      class="position-absolute"
-      style="
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        background-image: url('/src/imgs/equipments.jpg');
-        background-position: center center;
-        opacity: 0.8;
-        background-repeat: no-repeat;
-      "
-    ></div>
+    <img
+      src="/src/imgs/equipments.jpg"
+      alt=""
+      class="container"
+      style="width: 100%; height: 400px; object-fit: cover"
+    />
   </div>
   <div class="container mt-md-5 mt-3 mb-7">
     <div class="row">
